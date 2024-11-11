5cfbb5b49bfa6c6b87439aa90ee2af2f0d04838a
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-11-11 18:19:57 +0100

    Add brute force discrete solver

---
+++ mapc_research/brute_force/run.py
@@ -0,0 +1,82 @@
+import json
+from argparse import ArgumentParser
+from functools import partial
+from itertools import chain, combinations, product
+from typing import Iterable
+
+import jax
+import jax.numpy as jnp
+from mapc_sim.sim import network_data_rate
+from mapc_sim.constants import DEFAULT_TX_POWER
+from tqdm import tqdm
+
+from mapc_research.envs.static_scenario import StaticScenario
+from mapc_research.envs.test_scenarios import ALL_SCENARIOS
+
+
+def iter_tx(associations: dict) -> Iterable:
+    aps = set(associations)
+
+    for active in chain.from_iterable(combinations(aps, r) for r in range(1, len(aps) + 1)):
+        for stations in product(*((s for s in associations[a]) for a in active)):
+            yield tuple(zip(active, stations))
+
+
+def run_brute_force(scenario: StaticScenario, delta_tx_power: float = 3.0, tx_power_levels: int = 4) -> tuple:
+    associations = scenario.get_associations()
+    n_nodes = len(scenario.pos)
+
+    data_rate_fn = jax.jit(partial(
+        network_data_rate,
+        pos=scenario.pos,
+        mcs=None,
+        sigma=0.0,
+        walls=scenario.walls
+    ))
+    best_val, best_conf = 0, None
+
+    for conf in iter_tx(associations):
+        for power in product(range(tx_power_levels), repeat=len(conf)):
+            tx_matrix = jnp.zeros((n_nodes, n_nodes), dtype=int)
+            tx_power_matrix = jnp.zeros(n_nodes, dtype=int)
+
+            for ap, sta in conf:
+                tx_matrix = tx_matrix.at[ap, sta].set(1)
+
+            for (ap, _), tx in zip(conf, power):
+                tx_power_matrix = tx_power_matrix.at[ap].set(tx)
+
+            thr = data_rate_fn(jax.random.PRNGKey(0), tx_matrix, tx_power=DEFAULT_TX_POWER - delta_tx_power * tx_power_matrix)
+
+            if thr > best_val:
+                best_val, best_conf = thr, (conf, power)
+
+    return best_conf, best_val.item()
+
+
+if __name__ == '__main__':
+    args = ArgumentParser()
+    args.add_argument('-o', '--output', type=str, default='all_results.json')
+    args = args.parse_args()
+
+    all_results = []
+
+    for scenario in tqdm(ALL_SCENARIOS, desc='Scenarios'):
+        scenario_results = []
+        split_rate, split_conf = [], []
+
+        for static, _ in scenario.split_scenario():
+            conf, rate = run_brute_force(static)
+            split_conf.append(conf)
+            split_rate.append(rate)
+
+        scenario_results.append({
+            'agent': 'brute_force',
+            'conf': split_conf,
+            'runs': split_rate
+        })
+
+        all_results.append(scenario_results)
+
+    with open(args.output, 'w') as file:
+        json.dump(all_results, file)
