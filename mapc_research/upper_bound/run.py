4ba0b668c5f3cf8e38d6f35d32bce76b66f72643
Wojciech Ciezobka <wciezobka@agh.edu.pl>
2024-11-12 20:16:41 +0100

    Added link rates to the result json output file

--- mapc_research/upper_bound/run.py
+++ mapc_research/upper_bound/run.py
@@ -13,6 +13,19 @@ from mapc_research.envs.static_scenario import StaticScenario
 from mapc_research.envs.test_scenarios import ALL_SCENARIOS
 
 
+def convert_to_string(data: list) -> str:
+    new_data = []
+    for item in data:
+        new_item = {}
+        for key, inner_dict in item.items():
+            new_inner_dict = {}
+            for k, v in inner_dict.items():
+                new_inner_dict[str(k)] = v  # Convert tuple keys to strings
+            new_item[key] = new_inner_dict
+        new_data.append(new_item)
+    return new_data
+
+
 def run_solver(scenario: StaticScenario, solver_kwargs: dict) -> tuple:
     associations = scenario.get_associations()
     access_points = list(associations.keys())
@@ -39,16 +52,19 @@ if __name__ == '__main__':
         for opt_sum, opt_name in zip([True, False], ['opt_sum', 'opt_min']):
             split_rate = []
             split_shares = []
+            split_links_rates = []
 
             for static, _ in scenario.split_scenario():
                 configuration, solver_rate = run_solver(static, {'opt_sum': opt_sum})
                 split_rate.append(solver_rate)
                 split_shares.append(configuration["shares"])
+                split_links_rates.append(configuration["link_rates"])
 
             scenario_results.append({
                 'agent': opt_name,
                 'runs': split_rate,
-                'shares': split_shares
+                'shares': split_shares,
+                'link_rates': convert_to_string(split_links_rates)
             })
 
         all_results.append(scenario_results)
